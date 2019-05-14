d3767b4333095d52eb30287dc3ec4933c1ef9a93
Harry White <harry.white424@gmail.com>
2019-05-15 09:27:38 +1000

    Fixes to install script

--- install.sh
+++ install.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 VIM_PLUGINS_DIR=~/.vim/plugged
-VIM_PLUG_DIR=~/.vim/autoload/plug.vim
+VIM_PLUG_FILE=~/.vim/autoload/plug.vim
 # Clean plugins if found
 if [ -d $VIM_PLUGINS_DIR ]; then
     echo "Removing installed plugins..."
     rm -r $VIM_PLUGINS_DIR
 fi
 # Install vim-plug if required
-if [ ! -f $VIM_PLUG_DIR ]; then
+if [ ! -f $VIM_PLUG_FILE ]; then
     echo "Installing vim-plug..."
     # Install vim-plug
-    curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    curl -fLo $VIM_PLUG_FILE --create-dirs \
         https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 fi
 # Copy vimrc
@@ -18,5 +18,5 @@ echo "Copying vimrc..."
 cp --backup vimrc ~/.vimrc
 # Run PlugInstall then quit
 echo "Installing plugins..."
-# vim +PlugInstall +qall
+vim +PlugInstall +qall
 echo "Done!"
