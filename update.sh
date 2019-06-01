b7a6bde79538d7d482fe0708f2c455b103fbc812
Harry White <harry.white424@gmail.com>
2019-06-01 12:14:03 +1000

    Better updating script

--- update.sh
+++ update.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
-DOTFILES=$(find -name '.*' -type f)
-echo "Copying\n$DOTFILES"
-cp --backup $DOTFILES ~
-echo "Copying git template"
+echo "Copying files.."
+cp --backup .vimrc ~
+cp --backup .bashrc ~
+cp --backup .tmux.conf ~
+# Merge gitconfig with current files first 3 lines
+mv ~/.gitconfig ~/.gitconfig~
+head ~/.gitconfig~ -n 3 | cat - .gitconfig > ~/.gitconfig
+# Copy git template dir
 cp --backup -r ./.git_template ~
 echo "Done!"
 exit 0
