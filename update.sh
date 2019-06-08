bc28740c51d8a97ae50d70fc19f71e14cc4c82b1
Harry White <harry.white424@gmail.com>
2019-06-08 17:59:16 +1000

    Use long options in update script

--- update.sh
+++ update.sh
@@ -5,8 +5,8 @@ cp --backup .bashrc ~
 cp --backup .tmux.conf ~
 # Merge gitconfig with current files first 3 lines
 mv ~/.gitconfig ~/.gitconfig~
-head ~/.gitconfig~ -n 3 | cat - .gitconfig > ~/.gitconfig
+head ~/.gitconfig~ --lines=3 | cat - .gitconfig > ~/.gitconfig
 # Copy git template dir
-cp --backup -r ./.git_template ~
+cp --backup --recursive ./.git_template ~
 echo "Done!"
 exit 0
