c5cff37e28727fb08467fe33361fdf224fbaf0ce
Harry White <harry.white424@gmail.com>
2019-08-26 22:17:14 +1000

    Generic update script

--- update.sh
+++ update.sh
@@ -1,17 +1,55 @@
-#!/bin/sh
-echo "Copying files.."
-cp --backup .vimrc ~
-cp --backup .bashrc ~
-cp --backup .inputrc ~
-cp --backup .tmux.conf ~
+#!/bin/bash
+set -e
+
+OPTIONS="--help show help and exit\n--verbose print aditional info"
+USAGE="$0 [options]\nOptions:\n${OPTIONS}"
+VERBOSE=0
+
+# Flag arg parsing
+for i in $@; do
+    case $i in
+        "--help")
+            echo -e "$USAGE"
+            exit 0
+            ;;
+        "--verbose")
+            VERBOSE=1
+            ;;
+    esac
+done
+
+# Go to top of git repo
+TOP="$(git rev-parse --show-toplevel)"
+cd $TOP
+
+echo "Copying dotfiles..."
+
 # Merge gitconfig with current files first 3 lines
 mv ~/.gitconfig ~/.gitconfig~
 head ~/.gitconfig~ --lines=3 | cat - .gitconfig > ~/.gitconfig
-# Copy git template dir
-cp --backup --recursive ./.git_template ~
-# Copy .config dir
-cp --backup --recursive ./.config ~
-# Copy .vim
-cp --recursive ./.vim ~
+if [[ $VERBOSE -eq 1 ]]; then
+    echo "Copied ./.gitconfig"
+fi
+
+# Copy dotfiles
+DOTFILES="$(find . -maxdepth 1 -type f -regex '\./\..*' -a -not -name '.gitconfig')"
+for DF in $DOTFILES; do
+    cp --backup $DF $HOME
+    if [[ $VERBOSE -eq 1 ]]; then
+        echo "Copied $DF"
+    fi
+done
+
+echo "Copying dotdirs..."
+
+# Copy dot dirs
+DOTDIRS="$(find . -maxdepth 1 -type d -regex '\./\..*' -a -not -name '.git')"
+for DD in $DOTDIRS; do
+    cp --recursive $DD $HOME
+    if [[ $VERBOSE -eq 1 ]]; then
+        echo "Copied $DD"
+    fi
+done
+
 echo "Done!"
 exit 0
