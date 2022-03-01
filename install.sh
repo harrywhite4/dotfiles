899dae81aa7025ed731292e8f530b23905dbdf6e
Harry White <hwhite@atlassian.com>
2022-03-01 18:11:42 +1100

    Added option to uninstall symlinks

--- install.sh
+++ install.sh
@@ -2,9 +2,9 @@
 # Script to install dotfiles
 # Written in POSIX sh for compatibility
 set -e
-TODIR="${HOME}"
-VIM_PLUGINS_DIR=~/.vim/plugged
-VIM_PLUG_FILE=~/.vim/autoload/plug.vim
+todir="${HOME}"
+vim_plugins_dir=~/.vim/plugged
+vim_plug_file=~/.vim/autoload/plug.vim
 
 show_help() {
     cat << EOF
@@ -13,10 +13,12 @@ Install dotfiles
 
 Options:
 --help     Show help
+--un       Uninstall by removing symlinks
 --clean    Remove installed vim-plug plugins before installing
 EOF
 }
 
+uninstall=0
 clean=0
 
 # Flag arg parsing
@@ -29,13 +31,16 @@ for i in "$@"; do
         --clean)
             clean=1
             ;;
+        --un)
+            uninstall=1
+            ;;
     esac
 done
 
 # Clean plugins if required
-if [ -d "${VIM_PLUGINS_DIR}" ] && [ "${clean}" -eq 1 ]; then
+if [ -d "${vim_plugins_dir}" ] && [ "${clean}" -eq 1 ]; then
     echo "Removing installed vim plugins..."
-    rm -r "${VIM_PLUGINS_DIR}"
+    rm -r "${vim_plugins_dir}"
 fi
 
 # Backup an exiting file
@@ -53,7 +58,7 @@ backup_file() {
 # Create a symlink to file under current dir
 # Argument 1: Path to file / dir to be linked to (relative to current dir)
 # Argument 2: Path to directory link should be placed in
-makelink() {
+make_link() {
     linkname="${2}/${1}"
     if [ -h "${linkname}" ]; then
         echo "${linkname} was already a symlink"
@@ -66,44 +71,66 @@ makelink() {
     fi
 }
 
+# Remove a symlink if it exists
+# Same arguments as make_link above
+remove_link() {
+    linkname="${2}/${1}"
+    if [ -h "${linkname}" ]; then
+        rm "${linkname}"
+        echo "Removed symlink ${linkname}"
+    else
+        echo "Symlink ${linkname} doesn't exist"
+    fi
+}
+
+# Process a symlink by creating it if installing or deleting if uninstalling
+# Same arguments as make_link above
+process_link() {
+    if [ "${uninstall}" -eq 1 ]; then
+        remove_link "$1" "$2"
+    else
+        make_link "$1" "$2"
+    fi
+}
+
 # Symlink dotfiles
 find "${PWD}" -maxdepth 1 -type f -name '.*' | while read -r path; do
     file=$(basename "${path}")
-    makelink "${file}" "${TODIR}"
+    process_link "${file}" "${todir}"
 done
 
 # link from inside .vim
-mkdir -p "${TODIR}/.vim"
+mkdir -p "${todir}/.vim"
 for vpath in "${PWD}"/.vim/*; do
     vfile=$(basename "${vpath}")
-    makelink ".vim/${vfile}" "${TODIR}"
+    process_link ".vim/${vfile}" "${todir}"
 done
 
 # Link .config files
 find .config -type f | while read -r cpath; do
     cdir=$(dirname "${cpath}")
     # TODO: handle putting vscode config in ~/Library/Application Support/Code on Macos
-    mkdir -p "${TODIR}/${cdir}"
-    makelink "${cpath}" "${TODIR}"
+    mkdir -p "${todir}/${cdir}"
+    process_link "${cpath}" "${todir}"
 done
 
 # Symlinked directories
-makelink ".git_template" "${TODIR}"
-makelink ".pandoc" "${TODIR}"
+process_link ".git_template" "${todir}"
+process_link ".pandoc" "${todir}"
 
 if [ -x "$(command -v wget)" ]; then
     # Install vim-plug if required
-    if [ ! -f "${VIM_PLUG_FILE}" ]; then
+    if [ ! -f "${vim_plug_file}" ]; then
 	echo "Installing vim-plug..."
         # Create autoload dir
-	mkdir -p "$(dirname ${VIM_PLUG_FILE})"
+	mkdir -p "$(dirname ${vim_plug_file})"
 	# Install vim-plug
-	wget -O "${VIM_PLUG_FILE}" \
+	wget -O "${vim_plug_file}" \
 	    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
     fi
 
     # Run PlugInstall if required
-    if [ ! -d "${VIM_PLUGINS_DIR}" ]; then
+    if [ ! -d "${vim_plugins_dir}" ]; then
 	echo "Installing vim plugins..."
 	vim --not-a-term +PlugInstall +qall > /dev/null
     fi
